fc1527b015f04669d8734cafec968c02fdd92fe9
apollosaw <artemnubasik198@gmail.com>
2024-06-01 07:58:22 +0300

    Add files via upload

---
+++ servers.php
@@ -0,0 +1,42 @@
+[
+    {
+      "color": "ff368418",
+      "maxonline": 1000,
+      "name": "\u041c\u0410\u0422\u0420\u0415\u0428\u041a\u0410\u0020\u004d\u004f\u0042\u0049\u004c\u0045\u0020\u0023\u0031",
+      "online": 1000,
+      "recommend": true,
+      "edgar_host": "141.94.184.108",
+      "edgar_port": 1481,
+      "id": 1
+    },
+   {
+      "color": "30d5c8",
+      "maxonline": 1000,
+      "name": "\u041c\u0410\u0422\u0420\u0415\u0428\u041a\u0410\u0020\u004d\u004f\u0042\u0049\u004c\u0045\u0020\u0023\u0032",
+      "online": 841,
+      "recommend": true,
+      "edgar_host": "141.95.190.140",
+      "edgar_port": 1177,
+      "id": 2
+    },
+    {
+      "color": "ff0000",
+      "maxonline": 1000,
+      "name": "\u041c\u0410\u0422\u0420\u0415\u0428\u041a\u0410\u0020\u004d\u004f\u0042\u0049\u004c\u0045\u0020\u0023\u0033",
+      "online": 936,
+      "recommend": false,
+      "edgar_host": "185.189.15.22",
+      "edgar_port": 2003,
+      "id": 3
+    }, 
+    {
+      "color": "808080",
+      "maxonline": 1000,
+      "name": "\u0422\u0435\u0441\u0442\u0020\u0023\u0034",
+      "online": 0,
+      "recommend": false,
+      "edgar_host": "141.95.190.144",
+      "edgar_port": 1653,
+      "id": 4
+    }
+]
